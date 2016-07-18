6746305e32470500e599ad404992af757dada214
Paul MC Kim <kmc5500@naver.com>
2016-07-18 16:32:54 +0900

    add openGl test

--- Graphics/Graphics/opengl_test.cpp
+++ Graphics/Graphics/opengl_test.cpp
@@ -3,6 +3,8 @@
 void RenderScene(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glRectf(0.0f, 0.0f, 50.0f, 30.0f);
 	glFlush();
 }
 
@@ -11,11 +13,27 @@ void SetupRC(void)
    glClearColor(0.0f, 0.0f,1.0f,1.0f);
 }
 
+void ChangeSize(GLsizei w, GLsizei h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	if (w <= h)
+		glOrtho(-100.0f, 100.0f, -100.0f * h / w, 100.0f * h / w, 1.0f, -1.0f);
+	else
+		glOrtho(-100.0f * w / h, 100.0f * w / h, -100.0f, 100.0f, 1.0f, -1.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
 void main(void)
 {
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutCreateWindow("Simple");
 	glutDisplayFunc(RenderScene);
+	glutReshapeFunc(ChangeSize);
 	SetupRC();
 	glutMainLoop();
 }
