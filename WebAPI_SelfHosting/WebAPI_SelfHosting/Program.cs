d86f5dd35a5da8b24bb4f264571f10b3fa11a2e1
Paul MC Kim <kmc5500@naver.com>
2016-08-01 13:02:28 +0900

    add ASP.NET webapp

---
+++ WebAPI_SelfHosting/WebAPI_SelfHosting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Owin.Hosting;
+using System.Net.Http;
+
+namespace WebAPI_SelfHosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:19000/";
+
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                var client = new HttpClient();
+
+                var res = client.GetAsync(baseAddress + "api/values").Result;
+
+                Console.WriteLine(res);
+                Console.WriteLine(res.Content.ReadAsStringAsync().Result);
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ WebAPI_SelfHosting/WebAPI_SelfHosting/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+namespace WebAPI_SelfHosting
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(string id)
+        {
+            return "Hello, " + id + ".";
+        }
+
+        // POST api/values  BODY - name : 
+        public string Post([FromBody]string text)
+        {
+            Console.WriteLine("name : "+ text);
+            return "Hey," + text;
+        }
+    }
+}
