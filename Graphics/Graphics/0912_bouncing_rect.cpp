1eb36e12f0272b9da00c54e4f92ad2edd58b8b1c
Paul MC Kim <kmc5500@naver.com>
2016-09-13 03:24:05 +0900

    add phisycal collision

--- Graphics/Graphics/0912_bouncing_rect.cpp
+++ Graphics/Graphics/0912_bouncing_rect.cpp
@@ -14,19 +14,36 @@ struct MyRect
 	GLsizei size;
 	GLfloat xVelocity;
 	GLfloat yVelocity;
-	GLfloat bigColor[3] = {0.8f, 0.8f, 0.8f};
-	GLfloat smallColor[3] = {0.3f, 0.4f, 0.5f};
-	
+
+	GLfloat bigColorR = 0.8f;
+	GLfloat bigColorG = 0.8f;
+	GLfloat bigColorB = 0.8f;
+
+	GLfloat smallColorR = 0.3f;
+	GLfloat smallColorG = 0.4f;
+	GLfloat smallColorB = 0.5f;
+
 	MyRect() :x1(0), y1(0), size(10), xVelocity(1.0f), yVelocity(1.0f) {};
 
-	MyRect(GLfloat x, GLfloat y, GLsizei s, GLfloat xV, GLfloat yV) 
-		: x1(x), y1(y), size(s), xVelocity(xV), yVelocity(yV) {};
+	MyRect(GLfloat x, GLfloat y, GLsizei s, GLfloat xV, GLfloat yV,
+		GLfloat br, GLfloat bg, GLfloat bb)
+		: x1(x), y1(y), size(s), xVelocity(xV), yVelocity(yV),
+		bigColorR(br), bigColorG(bg), bigColorB(bb)
+	{};
+
+	MyRect(	GLfloat x, GLfloat y, GLsizei s, GLfloat xV, GLfloat yV, 
+			GLfloat br, GLfloat bg, GLfloat bb, 
+			GLfloat sr, GLfloat sg, GLfloat sb )
+		: x1(x), y1(y), size(s), xVelocity(xV), yVelocity(yV), 
+		bigColorR(br), bigColorG(bg), bigColorB(bb), 
+		smallColorR(sr), smallColorG(sg), smallColorB(sb)
+	{};
 
 	void Draw() 
 	{
-		glColor3f(bigColor[0], bigColor[1], bigColor[2]);
+		glColor3f(bigColorR, bigColorG, bigColorB);
 		glRectf(x1, y1, x1 + size, y1 + size);
-		glColor3f(smallColor[0], smallColor[1], smallColor[2]);
+		glColor3f(smallColorR, smallColorG, smallColorB);
 		glRectf(x1 + size / 4, y1 + size / 4, x1 + size * 3 / 4, y1 + size * 3 / 4);
 	}
 
@@ -79,7 +96,30 @@ bool CollisionProceed(MyRect& rv, MyRect& lv)
 
 	if (CollisionCheck(rv, lv))
 	{
-		float xCollisionDepth ;
+		//물리법칙 흉내 버전
+		float rvCenterX = rv.x1 + rv.size / 2.0f;
+		float lvCenterX = lv.x1 + lv.size / 2.0f;
+		float rvCenterY = rv.y1 + rv.size / 2.0f;
+		float lvCenterY = lv.y1 + lv.size / 2.0f;
+
+		float xForce = (rv.size + lv.size) / (rvCenterX - lvCenterX) *2.0f;
+		float yForce = (rv.size + lv.size) / (rvCenterY - lvCenterY) *2.0f;
+		
+		//적게 겹친 쪽만 적용.
+		if (std::abs(xForce) < std::abs(yForce)) {
+			rv.xVelocity += xForce / rv.size;
+			lv.xVelocity -= xForce / lv.size;
+		}
+		else {
+			rv.yVelocity += yForce / rv.size;
+			lv.yVelocity -= yForce / lv.size;
+		}
+		
+		rv.Move();
+		lv.Move();
+
+		//단순 충돌
+		/*float xCollisionDepth ;
 		if (rv.x1 + rv.size - lv.x1 > 0)
 			xCollisionDepth = rv.x1 + rv.size - lv.x1;
 		else if (lv.x1 + lv.size - rv.x1 > 0)
@@ -99,21 +139,34 @@ bool CollisionProceed(MyRect& rv, MyRect& lv)
 		{
 			rv.xVelocity = -rv.xVelocity;
 			lv.xVelocity = -lv.xVelocity;
-			rv.Move();
-			lv.Move();
+			if (rv.xVelocity > 0)
+			{
+				rv.Move(xCollisionDepth / 2.0f, 0);
+				lv.Move(-xCollisionDepth / 2.0f, 0);
+			}
+			else
+			{
+				rv.Move(-xCollisionDepth / 2.0f, 0);
+				lv.Move(xCollisionDepth / 2.0f, 0);
+			}
+			
 		}
 		else
 		{
 			rv.yVelocity = -rv.yVelocity;
 			lv.yVelocity = -lv.yVelocity;
-			rv.Move();
-			lv.Move();
-		}
+			if (rv.yVelocity > 0)
+			{
+				rv.Move(0, yCollisionDepth / 2.0f);
+				lv.Move(0, -yCollisionDepth / 2.0f);
+			}
+			else {
+				rv.Move(0, -yCollisionDepth / 2.0f);
+				lv.Move(0, yCollisionDepth / 2.0f);
+			}
+		}*/
 		return true;
 	}
-
-	rv.CollisionWindowBoundary();
-	lv.CollisionWindowBoundary();
 	return false;
 }
 
@@ -124,12 +177,14 @@ void RenderScene(void)
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	for (int i = 0; i < RECT_NUM; i++)
-		for (int j = i + 1; j < RECT_NUM; j++)
-			CollisionProceed(rectList[i], rectList[j]);
-	for (int i = 0; i < RECT_NUM; i++)
 	{
 		rectList[i].Move();
+		rectList[i].CollisionWindowBoundary();
 	}
+
+	for (int i = 0; i < RECT_NUM; i++)
+		for (int j = i + 1; j < RECT_NUM; j++)
+			CollisionProceed(rectList[i], rectList[j]);
 
 	for (int i = 0; i < RECT_NUM; i++)
 	{
@@ -149,10 +204,10 @@ void SetupRC(void)
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	GLfloat bigColor[3] = { 1,1,1 };
 	GLfloat smallColor[3] = { 0.5f, 0.5f, 0.5f };
-	rectList[0] = MyRect(0, 0, 40, 1.0f, 1.0f);
-	rectList[1] = MyRect(800 - 30, 600 - 30, 30, 1.0f, 1.0f);
-	rectList[2] = MyRect(800 - 30, 0, 50, 1.0f, 1.0f);
-	rectList[3] = MyRect(0, 600 - 30, 20, 1.0f, 1.0f);
+	rectList[0] = MyRect(0, 0, 40, .6f, .6f, .8f, .7f, .6f);
+	rectList[1] = MyRect(800 - 30, 600 - 30, 30, .6f, .6f, .5f, .7f, .9f);
+	rectList[2] = MyRect(800 - 30, 0, 50, .6f, .6f, .3f, .5f, .7f);
+	rectList[3] = MyRect(0, 600 - 30, 20, .6f, .6f, .4f, .7f, .6f);
 }
 
 void ChangeSize(GLsizei w, GLsizei h)
